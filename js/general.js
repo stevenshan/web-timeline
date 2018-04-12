695be0393247e0c452c68f25914dc3649862548d
Steven Shan <steven.shan99@gmail.com>
2018-04-12 19:24:10 -0400

    loading content in event boxes and styling for information in event box

--- js/general.js
+++ js/general.js
@@ -13,6 +13,21 @@ function randColor() {
 	return colors[randint(colors.length)];
 }
 
+function formatDate(date) {
+	var monthNames = [
+		"January", "February", "March",
+		"April", "May", "June", "July",
+		"August", "September", "October",
+		"November", "December"
+	];
+
+	var day = date.getDate();
+	var monthIndex = date.getMonth();
+	var year = date.getFullYear();
+
+	return monthNames[monthIndex] +  ' ' + day + ', ' + year;
+}
+
 $(function(){
 	finishedLoading();
 
@@ -26,13 +41,25 @@ $(function(){
 			container.append("<div id=\"timeline\"></div>")
 
 			data.forEach(function(e){
+				var date = new Date(e["date"]);
 				var dataClass = (counter % 2 == 0 ? "leftInfo" : "rightInfo")
+
+				links = "<h3>"
+				e["links"].forEach(function(link){
+					links += "<span><a href=\"https://" + link[1] + "\">" + 
+							 link[0] + "</a></span>";
+				});
+				links += "</h3>";
 
 				elem = "<div class=\"level event\">" + 
 						"<div class=\"infoDot\" style=\"background : " +
 						randColor() + "\"></div>" + 
 						"<div class=\"info " + dataClass + "\">" + 
-						"test" + 
+						"<div class=\"infoDate\">" +
+						formatDate(date) + "</div>" + 
+						"<h1>" + e["name"] + "</h1>" +
+						"<p>" + e["description"] + "</p>" + 
+						links +
 						"</div>" +
 						"</div>"
 				container.append(elem);
