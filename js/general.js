c915bc9c692fd487fcd465986046552a40d951ef
Steven Shan <steven.shan99@gmail.com>
2018-04-12 20:09:18 -0400

    added sorting dates, relevant year label dividers, and user options

--- js/general.js
+++ js/general.js
@@ -1,3 +1,10 @@
+// settings
+var RANDOM_COLORS = true; // randomize color of timeline labels
+var DEFAULT_COLOR = 0; // only works if RANDOM_COLORS is false
+var RANDOM_SIDES = true; // randomize side timeline events are on
+var CHRONOLOGICAL = true; // false for oldest first; true for newest first
+var DIVIDERS = true; // false for no year dividers; true for year dividers
+
 function finishedLoading()
 {
 	document.body.removeChild(document.getElementById("loadingMessage"));
@@ -5,15 +12,23 @@ function finishedLoading()
 
 var colors = ["#fcdd75", "#ca5454", "#e8a040", "#ebc59c", "#7e92b9"];
 
-function randint(max) {
+// get a random integer in the range [0, max)
+function randint(max) 
+{
 	return Math.floor(Math.random() * Math.floor(max));
 }
 
-function randColor() {
-	return colors[randint(colors.length)];
+// get a random color
+function randColor() 
+{
+	if (RANDOM_COLORS) return colors[randint(colors.length)];
+	else
+		return colors[DEFAULT_COLOR];
 }
 
-function formatDate(date) {
+// function to format date to Month Date, Year
+function formatDate(date) 
+{
 	var monthNames = [
 		"January", "February", "March",
 		"April", "May", "June", "July",
@@ -28,22 +43,71 @@ function formatDate(date) {
 	return monthNames[monthIndex] +  ' ' + day + ', ' + year;
 }
 
+// function to convert date strings to date objects
+function convertDates(data)
+{
+	for (i = 0; i < data.length; i++)
+	{
+		data[i]["date"] = new Date(data[i]["date"]);
+	}	
+	return data;
+}
+
 $(function(){
 	finishedLoading();
 
+	// get window dimensions
 	windowDim = {x: $(window).width(), y: $(window).height()};
 
+	// counter to make sure no more than 2 adjacent events go to same side
 	var counter = 0;
+
+	// asynchronous call to retrieve data
 	$.getJSON("projects.json", function(data){
+		// change date strings to date objects
+		data = convertDates(data);
+
+		// sort events by date
+		data.sort(function(a, b){
+			return (CHRONOLOGICAL ? a["date"] < b["date"] :
+									a["date"] > b["date"]);		
+		});
+
 		if (windowDim.x >= 900) // if desktop layout
 		{
 			var container = $("#container");
 			container.append("<div id=\"timeline\"></div>")
 
-			data.forEach(function(e){
-				var date = new Date(e["date"]);
-				var dataClass = (counter % 2 == 0 ? "leftInfo" : "rightInfo")
+			var lastYear = null;
 
+			// loop on each event to be added
+			data.forEach(function(e){
+				if (RANDOM_SIDES)
+				{
+					// random int to decide which side to put event on
+					var i = randint(2);
+
+					// ensure event isn't put on same side 3 times in a row
+					if (counter > 0 && i > 0) counter += 1;
+					else if (counter < 0 && i == 0) counter -= 1;
+					else counter = (i * 2 - 1);
+					if (Math.abs(counter) >= 3)
+					{
+						// flip i
+						i = (i == 0 ? 1 : 0);
+						counter = (i * 2 - 1);
+					}
+				}
+				else
+				{
+					i = counter;	
+					counter += 1;
+				}
+
+				var dataClass = (i % 2 == 0 ? 
+								 "leftInfo" : "rightInfo")
+
+				// generate html code for links section
 				links = "<h3>"
 				e["links"].forEach(function(link){
 					links += "<span><a href=\"https://" + link[1] + "\">" + 
@@ -56,17 +120,24 @@ $(function(){
 						randColor() + "\"></div>" + 
 						"<div class=\"info " + dataClass + "\">" + 
 						"<div class=\"infoDate\">" +
-						formatDate(date) + "</div>" + 
+						formatDate(e["date"]) + "</div>" + 
 						"<h1>" + e["name"] + "</h1>" +
 						"<p>" + e["description"] + "</p>" + 
 						links +
 						"</div>" +
-						"</div>"
-				container.append(elem);
-				container.append("<div class=\"divider level\" style=\"" + 
-					"background: " + randColor() + "\">2016</div>");
+						"</div>";
 
-				counter += 1;
+				// add generated html code to document
+				if (DIVIDERS && e["date"].getFullYear() != lastYear)
+				{
+					lastYear = e["date"].getFullYear();	
+					container.append("<div class=\"divider level\" style=\"" + 
+						"background: " + randColor() + "\">" +
+						lastYear + "</div>");
+				}
+
+				container.append(elem);
+
 			});
 		}
 	})
